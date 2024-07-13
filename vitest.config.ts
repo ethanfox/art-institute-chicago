1b4e7a8cb8632f83a3c6f17ea99c2a898433de4c
ethanfox <ethanj.fx@gmail.com>
2024-07-13 12:46:08 -0500

    Styled Navbar

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,22 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from "node:url";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import viteConfig from "./vite.config";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+    },
   })
-)
+);
