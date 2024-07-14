f770d2d37d764430417161c761407fd7c8aa8175
ethanfox <ethanj.fx@gmail.com>
2024-07-14 13:19:24 -0500

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-
-import Navbar from "@/components/Navbar.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
