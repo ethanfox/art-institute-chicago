a7dfc8e3ebea6316edc2afcc5de2a6d2b28aea6e
ethanfox <ethanj.fx@gmail.com>
2024-07-14 15:25:04 -0500

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,7 +14,7 @@
       <a
         class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 px-16 object-center w-full h-full text-center flex content-center transition-all"
         href="/visit"
-        ><span class="mx-auto my-auto">Visit</span></a
+        ><span class="mx-auto my-auto">Vijhghjgsit</span></a
       >
       <a
         class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 px-16 object-center w-full h-full text-center flex content-center transition-all"
