5afff023d64fea11c0718f8ad9f8c0f476f30ae2
ethanfox <ethanj.fx@gmail.com>
2024-07-14 12:54:39 -0500

    Created deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   base: "/art-institute-chicago/",
   plugins: [vue(), vueJsx(), vueDevTools()],
