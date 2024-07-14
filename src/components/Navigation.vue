7759700ed43c7bf8ab40e6fb6f18d87acf974e6b
ethanfox <ethanj.fx@gmail.com>
2024-07-14 16:04:26 -0500

    Fixed padding on nav buttons for mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -12,12 +12,12 @@
 
     <div class="w-full justify-between flex shadow-lg">
       <a
-        class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 px-16 object-center w-full h-full text-center flex content-center transition-all"
+        class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 p-4 lg:px-16 object-center w-full h-full text-center flex content-center transition-all"
         href="/visit"
         ><span class="mx-auto my-auto">Visit</span></a
       >
       <a
-        class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 px-16 object-center w-full h-full text-center flex content-center transition-all"
+        class="flex-p-8 my-auto mx-auto font-semibold text-lg hover:bg-neutral-200 p-4 lg:px-16 object-center w-full h-full text-center flex content-center transition-all"
         href="/about"
         ><span class="mx-auto my-auto">About</span></a
       >
