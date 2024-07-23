6959d0f8d1656f8fa463540cf5bfe5d6ef374806
ethanfox <ethanj.fx@gmail.com>
2024-07-23 07:43:21 -0500

    Added Shadcn

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,21 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 //https://vitejs.dev/config/
 export default defineConfig({
-    base: "/art-institute-chicago/",
-    plugins: [vue(), vueJsx(), vueDevTools()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
+  base: "/art-institute-chicago/",
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
+import path from "node:path";
 
 //https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
